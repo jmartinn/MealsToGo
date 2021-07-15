6552d55192df13b506d8d99081ce35a4d92cf331
juampemartin <juamp_m@hotmail.com>
2021-07-16 01:16:07 +0200

    A lot of features have been done

--- src/features/restaurants/screens/restaurants.screen.js
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
-import { FlatList } from 'react-native';
-import { Searchbar } from 'react-native-paper';
-import styled from 'styled-components';
-import { Spacer } from '../../../components/spacer/spacer.component';
+import React, { useContext } from "react";
+import { Searchbar, ActivityIndicator, Colors } from "react-native-paper";
+import { FlatList, View } from "react-native";
+import styled from "styled-components/native";
 
-import { RestaurantInfoCard } from '../components/restaurant-info-card.component';
-import { SafeArea } from '../../../components/utility/safe-area.component';
+import { SafeArea } from "../../../components/utility/safe-area.component";
+import { RestaurantInfoCard } from "../components/restaurant-info-card.component";
+import { Spacer } from "../../../components/spacer/spacer.component";
+
+import { RestaurantsContext } from "../../../services/restaurants/restaurants.context";
 
 const SearchContainer = styled.View`
   padding: ${(props) => props.theme.space[3]};
@@ -14,36 +16,57 @@ const SearchContainer = styled.View`
 const RestaurantList = styled(FlatList).attrs({
   contentContainerStyle: {
     padding: 16,
-  }
+  },
 })``;
 
-export const RestaurantsScreen = () => {
+const Loading = styled(ActivityIndicator)`
+  margin-left: -25px;
+`;
 
-  const [searchQuery, setSearchQuery] = useState('');
+const LoadingContainer = styled.View`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+`;
+
+export const RestaurantsScreen = () => {
+  const { restaurants, isLoading, error } = useContext(RestaurantsContext);
+
+  const [searchQuery, setSearchQuery] = React.useState('');
   const onChangeSearch = query => setSearchQuery(query);
 
-    return (
-      <SafeArea>
-        <SearchContainer>
-          <Searchbar placeholder="Buscar" onChangeText={onChangeSearch} value={searchQuery} />
-        </SearchContainer>
-        <FlatList 
-          data={[
-            { name: 1 }, 
-            { name: 2 },
-            { name: 3 },
-            { name: 4 },
-            { name: 5 },
-            { name: 6 } 
-          ]}
-          renderItem={() => (
+  return (
+    <SafeArea>
+      {isLoading && (
+        <LoadingContainer style={{ position: "absolute", top: "50%", left: "50%" }}>
+          <Loading
+            size={50}
+            style={{ marginLeft: -25 }}
+            animating={true}
+            color={"tomato"}
+           />
+        </LoadingContainer>
+      ) 
+
+      }
+      <SearchContainer>
+        <Searchbar
+          placeholder="Buscar"
+          onChangeText={onChangeSearch}
+          value={searchQuery}
+         />
+      </SearchContainer>
+      <RestaurantList
+        data={restaurants}
+        renderItem={({ item }) => {
+          return(
             <Spacer position="bottom" size="large">
-              <RestaurantInfoCard />
+              <RestaurantInfoCard restaurant={item} />
             </Spacer>
-          )}
-          keyExtractor={(item) => item.name}
-          contentContainerStyle={{ padding: 16 }}
-        />
-      </SafeArea>
-    );
-}
+          );
+        }}
+        keyExtractor={(item) => item.name}
+      />
+    </SafeArea>
+  );
+};
--- src/services/restaurants/restaurants.service.js
+++ src/services/restaurants/restaurants.service.js
@@ -1,25 +1,28 @@
-import { mocks } from "./mock/";
+import { mockImages, mocks } from "./mock";
 import camelize from "camelize";
-import { result } from "lodash";
-import { transform } from "async";
 
-export const restaurantsRequest = (location) => {
+export const restaurantsRequest = (location = "37.7749295,-122.4194155") => {
   return new Promise((resolve, reject) => {
     const mock = mocks[location];
-    if(!mock) {
-      reject("Not found");
+    if (!mock) {
+      reject("not found");
     }
     resolve(mock);
   });
 };
 
-const restaurantsTransform = (result) => {
-  return camelize(result);
-}
-restaurantsRequest().then(restaurantsTransform)
-.then(transformedResponse => {
-  console.log(transformedResponse);
-})
-.catch((err) => {
-  console.log(err)
-});
+export const restaurantsTransform = ({ results = [] }) => {
+  const mappedResults = results.map((restaurant) => {
+    restaurant.photos = restaurant.photos.map((p) => {
+      return mockImages[Math.ceil(Math.random() * (mockImages.length - 1))];
+    });
+
+    return {
+      ...restaurant,
+      isOpenNow: restaurant.opening_hours && restaurant.opening_hours.open_now,
+      isClosedTemporarily: restaurant.business_status === "CLOSED_TEMPORARILY",
+    };
+  });
+
+  return camelize(mappedResults);
+};
