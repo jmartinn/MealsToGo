a3885decaa4b112945031dae3e435b2ea3578111
juampemartin <juamp_m@hotmail.com>
2021-08-21 23:47:48 +0200

    fetching data

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,18 +1,16 @@
 import camelize from "camelize";
 
-import { locations } from "./location.mock";
-
 export const locationRequest = (searchTerm) => {
-  return new Promise((resolve, reject) => {
-    const locationMock = locations[searchTerm];
-    if (!locationMock) {
-      reject("not found");
-    }
-    resolve(locationMock);
+  return fetch(
+    `http://localhost:5001/mealstogo-a7c8e/us-central1/geocode?city=${searchTerm}`
+  ).then((res) => {
+    console.log(res.json);
+    return res.json();
   });
 };
 
 export const locationTransform = (result) => {
+  console.log(result);
   const formattedResponse = camelize(result);
   const { geometry = {} } = formattedResponse.results[0];
   const { lat, lng } = geometry.location;
