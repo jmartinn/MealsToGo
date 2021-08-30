b37bb53b03fb549eb461a4c17ca28fdec3abc7cc
Juan Pedro Martín <johnny@Juans-MacBook-Air.local>
2021-08-30 02:36:50 +0200

    Can now run completely locally

--- src/services/location/location.service.js
+++ src/services/location/location.service.js
@@ -1,9 +1,9 @@
 import camelize from "camelize";
-import { host } from "../../utils/env";
+import { host, isMock } from "../../utils/env";
 
 export const locationRequest = (searchTerm) => {
 	return fetch(
-		`${host}/geocode?city=${searchTerm}`,
+		`${host}/geocode?city=${searchTerm}&mock=${isMock}`,
 	).then((result) => {
 		return result.json();
 	});
--- src/utils/env.js
+++ src/utils/env.js
@@ -1,6 +1,9 @@
+import { Platform } from "react-native";
+
 const liveHost = "https://us-central1-mealstogo-a7c8e.cloudfunctions.net";
 const localHost = "http://35c1-217-61-224-91.ngrok.io/mealstogo-a7c8e/us-central1";
 
+export const isAndroid = Platform.OS === "android";
 export const isDevelopment = process.env.NODE_ENV === "development";
-
-export const host = isDevelopment ? localHost : liveHost;
+export const isMock = true;
+export const host = !isDevelopment || isAndroid ? liveHost : localHost;
